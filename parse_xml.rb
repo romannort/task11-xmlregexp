c4dfb0e395ab94ebb2f3adf844aeeffbbf56ddfd
Roman Ilienko <ilienko.roman@gmail.com>
2012-06-24 19:42:24 +0300

    Fixed: delimiters-only now valid string

--- parse_xml.rb
+++ parse_xml.rb
@@ -1,6 +1,6 @@
 def parse_xml( xml_string )
   xml_string.match( /\A
-    \s*<\s*volume\s*>\s*
+    \s*(<\s*volume\s*>\s*
     (
     (?'file_tag'\s*<\s*file\s*(name\s*=\s*".*")?\s*\/\s*>\s*
     )
@@ -9,5 +9,5 @@ def parse_xml( xml_string )
     (\g'file_tag'|\g'folder_tag')*
     \s*<\s*\/\s*folder\s*>\s*)
     )*
-    \s*<\s*\/\s*volume\s*>\s*\z/x ).to_s == xml_string
+    \s*<\s*\/\s*volume\s*>)*\s*\z/x ).to_s == xml_string
 end
