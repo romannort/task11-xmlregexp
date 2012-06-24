bf79f916b619b8152848542c1d239369443a4457
Roman Ilienko <ilienko.roman@gmail.com>
2012-06-24 19:01:35 +0300

    Changed file tag form.

--- parse_xml.rb
+++ parse_xml.rb
@@ -1,12 +1,12 @@
 def parse_xml( xml_string )
   xml_string.match( /\A
     \s*<\s*volume\s*>\s*
-    (?'file_tag_wrapper'
-    \s*<\s*file\s*(name=\".*\")?>\s*<\s*\/\s*file\s*>\s*
+    (?'file_tag'
+    \s*<\s*file\s*(name=\".*\")?\s*\/\s*>\s*
     )*
     (?'folder_tag'\s*<\s*folder\s*(name=\".*\")?>\s*
-    \g'file_tag_wrapper'*
+    \g'file_tag'*
     \g'folder_tag'*\s*<\s*\/\s*folder\s*>\s*)*
-    \g'file_tag_wrapper'*
+    \g'file_tag'*
     \s*<\s*\/\s*volume\s*>\s*\z/x  ).to_s == xml_string
 end
