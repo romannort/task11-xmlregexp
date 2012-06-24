a0b3270141484477e52b7b7d1383579167fc0734
Roman Ilienko <ilienko.roman@gmail.com>
2012-06-24 18:55:44 +0300

    Delete duplication in file tag detection part.

--- parse_xml.rb
+++ parse_xml.rb
@@ -2,11 +2,11 @@ def parse_xml( xml_string )
   xml_string.match( /\A
     \s*<\s*volume\s*>\s*
     (?'file_tag_wrapper'
-    (?'file_tag'\s*<\s*file\s*>\s*<\s*\/\s*file\s*>\s*)*
+    \s*<\s*file\s*(name=\".*\")?>\s*<\s*\/\s*file\s*>\s*
     )*
-    (?'folder_tag'\s*<\s*folder\s*>\s*
-    \g'file_tag_wrapper'
+    (?'folder_tag'\s*<\s*folder\s*(name=\".*\")?>\s*
+    \g'file_tag_wrapper'*
     \g'folder_tag'*\s*<\s*\/\s*folder\s*>\s*)*
-    \g'file_tag_wrapper'
+    \g'file_tag_wrapper'*
     \s*<\s*\/\s*volume\s*>\s*\z/x  ).to_s == xml_string
 end
