7af95fa21ac0eea5ad63ac77360809bf7295445b
Roman Ilienko <ilienko.roman@gmail.com>
2012-06-24 19:38:23 +0300

    Fixed bugs with folders after files. Now works fine %)

--- parse_xml.rb
+++ parse_xml.rb
@@ -1,12 +1,13 @@
 def parse_xml( xml_string )
   xml_string.match( /\A
     \s*<\s*volume\s*>\s*
-    (?'file_tag'
-    \s*<\s*file\s*(name=\".*\")?\s*\/\s*>\s*
+    (
+    (?'file_tag'\s*<\s*file\s*(name\s*=\s*".*")?\s*\/\s*>\s*
+    )
+    |
+    (?'folder_tag'\s*<\s*folder\s*(name\s*=\s*".*")?\s*>\s*
+    (\g'file_tag'|\g'folder_tag')*
+    \s*<\s*\/\s*folder\s*>\s*)
     )*
-    (?'folder_tag'\s*<\s*folder\s*(name=\".*\")?>\s*
-    \g'file_tag'*
-    \g'folder_tag'*\s*<\s*\/\s*folder\s*>\s*)*
-    \g'file_tag'*
-    \s*<\s*\/\s*volume\s*>\s*\z/x  ).to_s == xml_string
+    \s*<\s*\/\s*volume\s*>\s*\z/x ).to_s == xml_string
 end
